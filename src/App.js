fc936beb04662aa1998a0754d54b342f8465b504
Hassan Bulega <hassanbulega@gmail.com>
2022-06-18 15:56:25 +0300

    chore: add review component to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <main>
       <section className='container'>
         <div className='title'>
-          <h2>our reviews</h2>
+          <Review/>
           <div className='underline'></div>
         </div>
       </section>
